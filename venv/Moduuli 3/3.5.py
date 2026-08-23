06eb998f57e9b9469a8cb1db62d9b6087fa6e57b
Marquena <milo.pirttiniemi@yahoo.com>
2026-08-23 14:42:29 +0300

    mod3 done

---
+++ venv/Moduuli 3/3.5.py
@@ -0,0 +1,20 @@
+import math
+
+luoti = 13.3
+naula = luoti*32
+lieviskä = naula*20
+
+lievin = float(input("Anna lievisköjen määrä: "))
+naulain = float(input("Anna naulojen määrä: "))
+luotiin = float(input("anna luotien määrä: "))
+
+lievfin = lieviskä * lievin
+naulafin = naulain * naula
+luotifin = luotiin * luoti
+
+täys = lievfin + naulafin + luotifin
+gram, kilo = math.modf(täys/1000)
+
+print("Kokonaispaino moderneilla mittayksiköillä on: ")
+print(kilo, " Kilogrammaa")
+print(f"{gram*1000:10.1f}"," Grammaa")
---
+++ venv/Moduuli 3/3.6.py
@@ -0,0 +1,5 @@
+import random
+kk = random.randint(100,999)
+nk = random.randint(1000,6666)
+
+print(kk, nk)
