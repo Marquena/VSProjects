ef81fba2c6d91830fb9bd2980b77ec521f99dd9b
Marquena <milo.pirttiniemi@yahoo.com>
2026-08-24 21:47:27 +0300

    3.6korjaus

--- venv/Moduuli 3/3.6.py
+++ venv/Moduuli 3/3.6.py
@@ -3,10 +3,10 @@ k1 = random.randint(0,9)
 k2 = random.randint(0,9)
 k3 = random.randint(0,9)
 
-n1 = random.randint(0,6)
-n2 = random.randint(0,6)
-n3 = random.randint(0,6)
-n4 = random.randint(0,6)
+n1 = random.randint(1,6)
+n2 = random.randint(1,6)
+n3 = random.randint(1,6)
+n4 = random.randint(1,6)
 
 
 print(f"{k1}{k2}{k3}")
